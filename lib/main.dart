4654529071530ef3c510e3cdf3bdd908c6a1f641
AhmedWalid2005 <aw3694868@gmail.com>
2025-06-11 01:19:03 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const shopspheir());
+}
+
+class shopspheir extends StatelessWidget {
+  const shopspheir({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
